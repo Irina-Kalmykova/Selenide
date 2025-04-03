612b4c7f5978e4679cd3f44bd502296209adeae0
irina <1006-2017@mail.ru>
2025-04-03 23:56:43 +0400

    Initial commit

--- src/test/java/ru/netology/Selenide/CardDeliveryTest.java
+++ src/test/java/ru/netology/Selenide/CardDeliveryTest.java
@@ -1,6 +1,7 @@
 package ru.netology.Selenide;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -12,76 +13,79 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-
 public class CardDeliveryTest {
-    public static String setLocalDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy",
-                new Locale("ru")));
-    }
-    
-    @Test
-    void application() {
-        String date = setLocalDate(3);
+    @BeforeEach
+    void setUp() {
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Москва");
-        //$(byText("Выберите дату встречи с представителем банка")).click();
-        $("[data-test-id=date] input").doubleClick().sendKeys(date);
-        $("[data-test-id=name] input").setValue("Калмыкова Ирина");
-        $("[data-test-id=phone] input").setValue("+79275659123");
-        //$x("//*[text()='Я соглашаюсь с условиями обработки и использования моих персональных данных']").click();
-        $("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
-        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
-                Duration.ofSeconds(15)).shouldBe(visible);
     }
 
-    @Test
-    void cityWithHyphen() {
-        String date = setLocalDate(3);
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        $("[data-test-id=date] input").doubleClick().sendKeys(date);
-        $("[data-test-id=name] input").setValue("Калмыкова Ирина");
-        $("[data-test-id=phone] input").setValue("+79275659123");
-        $("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
-        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
-                Duration.ofSeconds(15)).shouldBe(visible);
-    }
+    @Nested
+    class cardDeliveryTest {
+        public String setLocalDate(int days) {
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy",
+                    new Locale("ru")));
+        }
 
-    @Test
-    void meetingAfterThreeDays() {
-        String date = setLocalDate(4);
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Москва");
-        //$(byText("Выберите дату встречи с представителем банка")).click();
-        $("[data-test-id=date] input").doubleClick().sendKeys(date);
-        $("[data-test-id=name] input").setValue("Калмыкова Ирина");
-        $("[data-test-id=phone] input").setValue("+79275659123");
-        //$x("//*[text()='Я соглашаюсь с условиями обработки и использования моих персональных данных']").click();
-        $("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
-        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
-                Duration.ofSeconds(15)).shouldBe(visible);
-    }
+        @Test
+        void application() {
+            String date = setLocalDate(3);
+            $("[data-test-id=city] input").setValue("Москва");
+            //$(byText("Выберите дату встречи с представителем банка")).click();
+            $("[data-test-id=date] input").doubleClick().sendKeys(date);
+            $("[data-test-id=name] input").setValue("Калмыкова Ирина");
+            $("[data-test-id=phone] input").setValue("+79275659123");
+            //$x("//*[text()='Я соглашаюсь с условиями обработки и использования моих персональных данных']").click();
+            $("[data-test-id=agreement]").click();
+            $$("button").find(exactText("Забронировать")).click();
+            $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+            $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                    Duration.ofSeconds(15)).shouldBe(visible);
+        }
 
-    @Test
-    void doubleSurname() {
-        String date = setLocalDate(3);
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Казань");
-        //$(byText("Выберите дату встречи с представителем банка")).click();
-        $("[data-test-id=date] input").doubleClick().sendKeys(date);
-        $("[data-test-id=name] input").setValue("Иванов-Купер Сергей");
-        $("[data-test-id=phone] input").setValue("+79370101813");
-        //$x("//*[text()='Я соглашаюсь с условиями обработки и использования моих персональных данных']").click();
-        $("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
-        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
-                Duration.ofSeconds(15)).shouldBe(visible);
+        @Test
+        void cityWithHyphen() {
+            String date = setLocalDate(3);
+            $("[data-test-id=city] input").setValue("Санкт-Петербург");
+            $("[data-test-id=date] input").doubleClick().sendKeys(date);
+            $("[data-test-id=name] input").setValue("Калмыкова Ирина");
+            $("[data-test-id=phone] input").setValue("+79275659123");
+            $("[data-test-id=agreement]").click();
+            $$("button").find(exactText("Забронировать")).click();
+            $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+            $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                    Duration.ofSeconds(15)).shouldBe(visible);
+        }
+
+        @Test
+        void meetingAfterThreeDays() {
+            String date = setLocalDate(4);
+            $("[data-test-id=city] input").setValue("Москва");
+            //$(byText("Выберите дату встречи с представителем банка")).click();
+            $("[data-test-id=date] input").doubleClick().sendKeys(date);
+            $("[data-test-id=name] input").setValue("Калмыкова Ирина");
+            $("[data-test-id=phone] input").setValue("+79275659123");
+            //$x("//*[text()='Я соглашаюсь с условиями обработки и использования моих персональных данных']").click();
+            $("[data-test-id=agreement]").click();
+            $$("button").find(exactText("Забронировать")).click();
+            $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+            $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                    Duration.ofSeconds(15)).shouldBe(visible);
+        }
+
+        @Test
+        void doubleSurname() {
+            String date = setLocalDate(3);
+            $("[data-test-id=city] input").setValue("Казань");
+            //$(byText("Выберите дату встречи с представителем банка")).click();
+            $("[data-test-id=date] input").doubleClick().sendKeys(date);
+            $("[data-test-id=name] input").setValue("Иванов-Купер Сергей");
+            $("[data-test-id=phone] input").setValue("+79370101813");
+            //$x("//*[text()='Я соглашаюсь с условиями обработки и использования моих персональных данных']").click();
+            $("[data-test-id=agreement]").click();
+            $$("button").find(exactText("Забронировать")).click();
+            $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(15000));
+            $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                    Duration.ofSeconds(15)).shouldBe(visible);
+        }
     }
 }
